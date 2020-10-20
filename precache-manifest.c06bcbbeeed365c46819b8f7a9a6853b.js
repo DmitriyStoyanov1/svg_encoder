4d0a7b9a8f9780f707e4dec1344c10ca3f0bfb14
DmitriyStoyanov1 <DmitriyStoyanov87@gmail.com>
2020-10-20 22:42:00 +0300

    Updates

---
+++ precache-manifest.c06bcbbeeed365c46819b8f7a9a6853b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7248692cc0829a7e2b95ec7e63ab5ab8",
+    "url": "/svg_encoder/index.html"
+  },
+  {
+    "revision": "1f9f0abd6bf4d6123c6d",
+    "url": "/svg_encoder/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "d2d790c50e48e9899882",
+    "url": "/svg_encoder/static/css/main.b6d03f15.chunk.css"
+  },
+  {
+    "revision": "1f9f0abd6bf4d6123c6d",
+    "url": "/svg_encoder/static/js/2.6d9066ed.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/svg_encoder/static/js/2.6d9066ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2d790c50e48e9899882",
+    "url": "/svg_encoder/static/js/main.536b680b.chunk.js"
+  },
+  {
+    "revision": "c12deae043158662a634",
+    "url": "/svg_encoder/static/js/runtime-main.b0af7e66.js"
+  },
+  {
+    "revision": "c39480d514fe1af4c7e5f62a3ac53b67",
+    "url": "/svg_encoder/static/media/gb.c39480d5.svg"
+  },
+  {
+    "revision": "e2037a0ebc084965c2c2744c68bd5837",
+    "url": "/svg_encoder/static/media/ru.e2037a0e.svg"
+  },
+  {
+    "revision": "cf1a4bc62040aab5017924cd7d217155",
+    "url": "/svg_encoder/static/media/ua.cf1a4bc6.svg"
+  }
+]);
